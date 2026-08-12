b2563132610f07c9d79df806f32eefe7e2744f23
Made In Heaven <madeinheaven@madeinheaven.com>
2026-08-12 20:58:39 +1000

    Prove predicate regex acceptance complete

--- test/cure/stdlib/dependent_regex_language_correctness_test.exs
+++ test/cure/stdlib/dependent_regex_language_correctness_test.exs
@@ -26,11 +26,27 @@ defmodule Cure.Stdlib.DependentRegexLanguageCorrectnessTest do
       acceptance
     )
 
+    fn predicate_completeness(
+      denotation: PatternDenotation(CharC, PatternPredicate(accepts_a), subject_initial_position(), Cons('a', no_chars()), no_chars())
+    ) -> PredicateAcceptance(accepts_a, 'a', no_chars(), subject_initial_position()) = predicate_denotation_is_complete(
+      accepts_a,
+      'a',
+      no_chars(),
+      subject_initial_position(),
+      denotation
+    )
+
   """
 
   test "predicate denotation is sound for certified execution" do
     assert {:ok, env} = Program.elaborate(@source)
     assert Env.total?(env, :predicate_soundness)
     assert Env.total?(env, :"Std.Regex.Language#predicate_acceptance_is_sound")
+  end
+
+  test "predicate denotation constructively produces certified execution" do
+    assert {:ok, env} = Program.elaborate(@source)
+    assert Env.total?(env, :predicate_completeness)
+    assert Env.total?(env, :"Std.Regex.Language#predicate_denotation_is_complete")
   end
 end
