78c9a717608bd1ff56aac5ff3897005dc58e0626
Made In Heaven <madeinheaven@madeinheaven.com>
2026-08-18 14:09:54 +1000

    return captured strings for line breaks

--- test/cure/stdlib/dependent_regex_unicode_name_test.exs
+++ test/cure/stdlib/dependent_regex_unicode_name_test.exs
@@ -12,16 +12,16 @@ defmodule Cure.Stdlib.DependentRegexUnicodeNameTest do
       fn latin_capital_a(input: String) -> Option(Unit) = parse_full(/\N{LATIN CAPITAL LETTER A}/, input)
       fn euro(input: String) -> Option(Unit) = parse_full(/\N{EURO SIGN}/, input)
       fn control_alias(input: String) -> Option(Unit) = parse_full(/\N{LINE FEED}/, input)
-      fn line_feed(input: String) -> Option(Unit) = parse_full(/\R/, input)
-      fn carriage_return_line_feed(input: String) -> Option(Unit) = parse_full(/\R/, input)
-      fn unicode_line_separator(input: String) -> Option(Unit) = parse_full(/\R/, input)
-      fn lf_policy(input: String) -> Option(Unit) = parse_full(/(*LF)\R/, input)
-      fn cr_policy(input: String) -> Option(Unit) = parse_full(/(*CR)\R/, input)
-      fn crlf_policy(input: String) -> Option(Unit) = parse_full(/(*CRLF)\R/, input)
-      fn anycrlf_policy(input: String) -> Option(Unit) = parse_full(/(*ANYCRLF)\R/, input)
-      fn any_policy(input: String) -> Option(Unit) = parse_full(/(*ANY)\R/, input)
-      fn bsr_anycrlf_policy(input: String) -> Option(Unit) = parse_full(/(*BSR_ANYCRLF)\R/, input)
-      fn bsr_unicode_policy(input: String) -> Option(Unit) = parse_full(/(*BSR_UNICODE)\R/, input)
+      fn line_feed(input: String) -> Option(String) = parse_full(/\R/, input)
+      fn carriage_return_line_feed(input: String) -> Option(String) = parse_full(/\R/, input)
+      fn unicode_line_separator(input: String) -> Option(String) = parse_full(/\R/, input)
+      fn lf_policy(input: String) -> Option(String) = parse_full(/(*LF)\R/, input)
+      fn cr_policy(input: String) -> Option(String) = parse_full(/(*CR)\R/, input)
+      fn crlf_policy(input: String) -> Option(String) = parse_full(/(*CRLF)\R/, input)
+      fn anycrlf_policy(input: String) -> Option(String) = parse_full(/(*ANYCRLF)\R/, input)
+      fn any_policy(input: String) -> Option(String) = parse_full(/(*ANY)\R/, input)
+      fn bsr_anycrlf_policy(input: String) -> Option(String) = parse_full(/(*BSR_ANYCRLF)\R/, input)
+      fn bsr_unicode_policy(input: String) -> Option(String) = parse_full(/(*BSR_UNICODE)\R/, input)
       fn lf_dot(input: String) -> Option(Char) = parse_full(/(*LF)./, input)
       fn crlf_dot(input: String) -> Option(Char) = parse_full(/(*CRLF)./, input)
       fn crlf_anchor(input: String) -> Option(Match(Unit)) = search(/(*CRLF)a$/m, input)
@@ -40,32 +40,32 @@ defmodule Cure.Stdlib.DependentRegexUnicodeNameTest do
   end
 
   test "R matches CRLF as one finite line-break alternative and Unicode separators", %{runtime_module: module} do
-    assert apply(module, :line_feed, [{:String, [10]}]) == {:some, :unit}
-    assert apply(module, :carriage_return_line_feed, [{:String, [13, 10]}]) == {:some, :unit}
-    assert apply(module, :unicode_line_separator, [{:String, [0x2028]}]) == {:some, :unit}
+    assert apply(module, :line_feed, [{:String, [10]}]) == {:some, {:String, [10]}}
+    assert apply(module, :carriage_return_line_feed, [{:String, [13, 10]}]) == {:some, {:String, [13, 10]}}
+    assert apply(module, :unicode_line_separator, [{:String, [0x2028]}]) == {:some, {:String, [0x2028]}}
     assert apply(module, :line_feed, [{:String, [13, 10, 13]}]) == :none
   end
 
   test "newline controls select finite R policies", %{runtime_module: module} do
-    assert apply(module, :lf_policy, [{:String, [10]}]) == {:some, :unit}
+    assert apply(module, :lf_policy, [{:String, [10]}]) == {:some, {:String, [10]}}
     assert apply(module, :lf_policy, [{:String, [13]}]) == :none
     assert apply(module, :lf_policy, [{:String, [13, 10]}]) == :none
 
-    assert apply(module, :cr_policy, [{:String, [13]}]) == {:some, :unit}
+    assert apply(module, :cr_policy, [{:String, [13]}]) == {:some, {:String, [13]}}
     assert apply(module, :cr_policy, [{:String, [10]}]) == :none
 
-    assert apply(module, :crlf_policy, [{:String, [13, 10]}]) == {:some, :unit}
+    assert apply(module, :crlf_policy, [{:String, [13, 10]}]) == {:some, {:String, [13, 10]}}
     assert apply(module, :crlf_policy, [{:String, [13]}]) == :none
     assert apply(module, :crlf_policy, [{:String, [10]}]) == :none
 
-    assert apply(module, :anycrlf_policy, [{:String, [13]}]) == {:some, :unit}
-    assert apply(module, :anycrlf_policy, [{:String, [10]}]) == {:some, :unit}
-    assert apply(module, :anycrlf_policy, [{:String, [13, 10]}]) == {:some, :unit}
+    assert apply(module, :anycrlf_policy, [{:String, [13]}]) == {:some, {:String, [13]}}
+    assert apply(module, :anycrlf_policy, [{:String, [10]}]) == {:some, {:String, [10]}}
+    assert apply(module, :anycrlf_policy, [{:String, [13, 10]}]) == {:some, {:String, [13, 10]}}
     assert apply(module, :anycrlf_policy, [{:String, [0x2028]}]) == :none
 
-    assert apply(module, :any_policy, [{:String, [0x2028]}]) == {:some, :unit}
+    assert apply(module, :any_policy, [{:String, [0x2028]}]) == {:some, {:String, [0x2028]}}
     assert apply(module, :bsr_anycrlf_policy, [{:String, [0x2028]}]) == :none
-    assert apply(module, :bsr_unicode_policy, [{:String, [0x2028]}]) == {:some, :unit}
+    assert apply(module, :bsr_unicode_policy, [{:String, [0x2028]}]) == {:some, {:String, [0x2028]}}
   end
 
   test "newline policies also govern multiline anchors", %{runtime_module: module} do
