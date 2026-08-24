452fe8b1399554e07161ffdb1a73645a866ea9b8
Made In Heaven <madeinheaven@madeinheaven.com>
2026-08-24 12:06:43 +1000

    Split atomic root and escaped no-results

--- test/cure/stdlib/dependent_regex_assertion_decision_test.exs
+++ test/cure/stdlib/dependent_regex_assertion_decision_test.exs
@@ -622,6 +622,8 @@ defmodule Cure.Stdlib.DependentRegexAssertionDecisionTest do
     assert source =~ "type AtomicPathRootRefutation"
     assert source =~ "AtomicPathSearchRootActiveNo"
     assert source =~ "AtomicPathSearchRootExactNo"
+    assert source =~ "AtomicPathSearchRootNo"
+    assert source =~ "atomic_path_members_root_to_search"
     assert source =~ "atomic_path_root_exact_accepted_with_input_evidence"
     assert source =~ "atomic_path_root_input_exhausted_evidence"
     assert Regex.match?(~r/AtomicPathExactAcceptedWithInput\s*:[^\n]*Cons\(char, rest\)/, source)
